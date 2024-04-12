c2f90c838ee1ae08cecc2c2931f085916a80ec00
Mohamed De Franceschi <mohamed.de-franceschi@etu.unistra.fr>
2024-04-13 01:15:38 +0200

    Menu project coulissant horizontalement presque fini juste quelque bug

--- js/script.js
+++ js/script.js
@@ -23,6 +23,23 @@ window.addEventListener("mousemove", function(e){
 });
 
 
+//change la taille du curseur quand on click
+function adjustDotSize(dot1size, dote2size) {
+    dot.style.transition = 'height 0.3s, width 0.3s';
+    dot2.style.transition = 'height 0.3s, width 0.3s';
+    dot.style.height = dot1size;
+    dot.style.width = dot1size;
+    dot2.style.height = dote2size;
+    dot2.style.width = dote2size;
+}
+document.addEventListener('mousedown', function() {
+    adjustDotSize('20px', '30px');
+});
+document.addEventListener('mouseup', function() {
+    adjustDotSize('10px', '50px');
+});
+
+
 
 
 //                         Theme sombre theme claire
@@ -104,6 +121,67 @@ function scrollToMiddle(sectionId, currentPage) {
         behavior: 'smooth'
     });
 }
+
+
+//                       Project Scroll
+
+const carousel = document.querySelector(".carousel");
+var dot = document.querySelector('.cursor-dot');
+var dot2 = document.querySelector('.cursor-outline');
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false, startX, startScrollLeft;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+carouselChildrens.slice(0, cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+arrowBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+    })
+});
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const dragging = (e) => {
+    if(!isDragging) return
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const infiniteScroll = () => {
+    if(carousel.scrollLeft === 0){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+        carousel.classList.remove("no-transition");
+    }else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+    }
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+
 
 
 
--- project.php
+++ project.php
@@ -13,39 +13,47 @@
     <?php include 'templates/header.php';?>
     <!-- / -->
 
-    <section class="ProjectHere">
+    <!--<section class="ProjectHere">
         <p class="ProjectTxt">All my project</p>
         <p class="ProjectTxt">here</p>
         <button class=ProjectButton>Click to scroll through my projects</button>
-    </section>
+    </section>-->
 
     <section class="ProjectScroll">
         <div class="wrapper">
-            <i class="fa-solid fa-angle-left"></i>
+            <i id="left" class="fa-solid fa-angle-left"></i>
             <ul class="carousel">
                 <li class="card">
-                    <div class="img"><img src="img/sokoban.png" alt="sokoban"></div>
+                    <div class="img"><img src="img/sokoban.png" alt="sokoban" draggable="false"></div>
                     <h2>Sokoban Game</h2>
                     <span>Language C</span>
                 </li>
                 <li class="card">
-                    <div class="img"><img src="img/spaceinvader.jpeg" alt="space invader"></div>
+                    <div class="img"><img src="img/spaceinvader.jpeg" alt="space invader" draggable="false"></div>
                     <h2>Space Invader</h2>
                     <span>Language Risk V</span>
                 </li>
                 <li class="card">
-                    <div class="img"><img src="img/studium.jpg" alt="unistra bibliotheque"></div>
+                    <div class="img"><img src="img/studium.jpg" alt="unistra bibliotheque" draggable="false"></div>
                     <h2>Web S2</h2>
                     <span>Language HTML CSS JS</span>
                 </li>
+                <li class="card">
+                    <div class="img"><img src="img/portfolio.jpg" alt="portoflio" draggable="false"></div>
+                    <h2>Web S2</h2>
+                    <span>Language HTML CSS JS</span>
+                </li>
+                <li class="card">
+                    <div class="img"><img src="img/ue5.png" alt="ue5 3d" draggable="false"></div>
+                    <h2>UE5 game</h2>
+                    <span>UE5 and Blender</span>
+                </li>
             </ul>
-            <i class="fa-solid fa-angle-right"></i>
+            <i id="right" class="fa-solid fa-angle-right"></i>
         </div>  
     </section>
 
-    <!-- https://www.youtube.com/watch?v=6QE8dXq9SOE -->
-    <!-- Pour l'appel ajax prendre exemple de : https://git.unistra.fr/fbonjour-progweb2/projet-zombie-l2s4ptd2/-/blob/main/assets/js/search_form.js?ref_type=heads -->
-
+    
     <!-- Footer -->
     <?php include 'templates/footer.php';?>
     <!-- / -->
